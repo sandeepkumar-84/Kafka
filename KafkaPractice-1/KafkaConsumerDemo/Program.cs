66061aec72a43d46df2a5620f6156188a272850b
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2025-11-03 19:12:53 +0000

    Add project files.

---
+++ KafkaPractice-1/KafkaConsumerDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092,localhost:9093",
+    GroupId = "demo-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe("test-topic");
+
+Console.WriteLine("Listening for messages... Press Ctrl+C to exit.");
+
+try
+{
+    while (true)
+    {
+        var cr = consumer.Consume();
+        Console.WriteLine($"Received: {cr.Message.Value}");
+    }
+}
+catch (OperationCanceledException)
+{
+    consumer.Close();
+}
+    
---
+++ KafkaPractice-1/KafkaProducerDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092,localhost:9093"
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+Console.WriteLine("Enter messages (type 'exit' to quit):");
+
+
+while (true)
+{
+    var message = Console.ReadLine();
+    if (message == "exit") break;
+
+    var result = await producer.ProduceAsync("test-topic", new Message<Null, string> { Value = message });
+    Console.WriteLine($"Delivered '{result.Value}' to: {result.TopicPartitionOffset}");
+}
